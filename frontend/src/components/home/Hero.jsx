4cd467e89e52f4edc7800be8ba88bb2e9dbdc1fa
Aman Tiwari <amantdevio@gmail.com>
2026-02-22 11:50:54 +0530

    Removed menu beccause no need the menu in website

--- frontend/src/components/home/Hero.jsx
+++ frontend/src/components/home/Hero.jsx
@@ -16,13 +16,6 @@ const Hero = () => {
                        <img src="/logo.svg" alt="logo" className='h-11 w-auto' />
                     </a>
 
-                    <div className="hidden md:flex items-center gap-8 transition duration-500 text-slate-800">
-                        <a href="#" className="hover:text-green-600 transition">Home</a>
-                        <a href="#features" className="hover:text-green-600 transition">Features</a>
-                        <a href="#testimonials" className="hover:text-green-600 transition">Testimonials</a>
-                        <a href="#cta" className="hover:text-green-600 transition">Contact</a>
-                    </div>
-
                     <div className="flex gap-2">
                         <Link to='/app?state=register' className="hidden md:block px-6 py-2 bg-green-500 hover:bg-green-700 active:scale-95 transition-all rounded-full text-white" hidden={user}>
                             Get started
@@ -34,23 +27,7 @@ const Hero = () => {
                         <Link to='/app' className='hidden md:block px-8 py-2 bg-green-500 hover:bg-green-700 active:scale-95 transition-all rounded-full text-white' hidden={!user}>Dashboard</Link>
                     </div>
 
-                    <button onClick={() => setMenuOpen(true)} className="md:hidden active:scale-90 transition" >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="none" stroke="currentColor" strokeWidth="2" className="lucide lucide-menu" >
-                            <path d="M4 5h16M4 12h16M4 19h16" />
-                        </svg>
-                    </button>
                 </nav>
-
-                {/* Mobile Menu */}
-                <div className={`fixed inset-0 z-[100] bg-black/40 text-black backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-300 ${menuOpen ? "translate-x-0" : "-translate-x-full"}`} >
-                    <a href="#" className="text-white" onClick={() => setMenuOpen(false)}>Home</a>
-                    <a href="#features" className="text-white" onClick={() => setMenuOpen(false)}>Features</a>
-                    <a href="#testimonials" className="text-white" onClick={() => setMenuOpen(false)}>Testimonials</a>
-                    <a href="#cta" className="text-white" onClick={() => setMenuOpen(false)}>Contact</a>
-                    <button onClick={() => setMenuOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-green-600 hover:bg-green-700 transition text-white rounded-md flex" >
-                        X
-                    </button>
-                </div>
 
                 {/* Hero Section */}
                 <div className="relative flex flex-col items-center justify-center text-sm px-4 md:px-16 lg:px-24 xl:px-40 text-black">
